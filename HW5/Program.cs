239b57878c15d36ef72627507e150b2dce750249
konul1999 <konulfma@code.edu.az>
2025-07-28 18:08:42 +0400

    Add project files.

---
+++ HW5/Program.cs
@@ -0,0 +1,73 @@
+﻿//namespace HW5
+//{
+//    internal class Program
+    //{
+        //static void Main(string[] args)
+        //{
+
+        //    int[] arr = { 1, -2, 3 };
+        //    Console.WriteLine(GetMin(arr));
+        //}
+        //public static int GetMin(int[] array)
+        //{
+        //    int min = array[0];
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        if (array[i] < min)
+        //        {
+        //            min = array[i];
+        //        }
+        //    }
+        //    return min;
+        //}
+
+    //
+    //
+    //   static void Main(string[] args)
+    //    {
+    //        int[] arr = { 1, -2, 3 };
+
+    //        int[] sortedArr = SortArray(arr);
+
+    //        Console.WriteLine("Sıralanmış array:");
+    //        foreach (int num in sortedArr)
+    //        {
+    //            Console.Write(num + " ");
+    //        }
+    //        Console.WriteLine();
+    //    }
+
+    //    public static int[] SortArray(int[] array)
+    //    {
+    //        for (int i = 0; i < array.Length - 1; i++)
+    //        {
+    //            for (int j = 0; j < array.Length - i - 1; j++)
+    //            {
+    //                if (array[j] > array[j + 1])
+    //                {
+    //                    int temp = array[j];
+    //                    array[j] = array[j + 1];
+    //                    array[j + 1] = temp;
+    //                }
+    //            }
+    //        }
+    //        return array;
+    //    }
+    //}
+
+
+
+    //public static int GetPower(int baseNumber, int power = 2)
+    //    {
+    //        int result = 1;
+
+    //        for (int i = 0; i < power; i++)
+    //        {
+    //            result *= baseNumber;
+    //        }
+
+    //        return result;
+    //    }
+
+    //}
+}
